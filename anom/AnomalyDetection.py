a6b801cc3879219aff7b6359c8c4dfbd661bb551
Arie Agranonik <arieag@gmail.com>
2015-07-13 11:00:23 +0300

    anomaly longterm addition

--- anom/AnomalyDetection.py
+++ anom/AnomalyDetection.py
@@ -6,7 +6,9 @@ Created on 9/07/2015
 
 import pandas as pd
 import warnings
-
+import math
+import numpy as np
+import datetime
 
 def get_gran(tseries):
 
@@ -105,8 +107,30 @@ class AnomalyDetection(object):
         if max_anoms < 1/num_obs:
             max_anoms = 1/num_obs
 
+        all_data = dict()
+        if longterm:
+
+            if gran == "day":
+                # STL needs 2*period + 1 observations
+                num_obs_in_period = period*piecewise_median_period_weeks + 1
+                num_days_in_period = (7*piecewise_median_period_weeks) + 1
+            else:
+                num_obs_in_period = period*7*piecewise_median_period_weeks
+                num_days_in_period = (7*piecewise_median_period_weeks)
 
 
+            last_date = x.index[-1]
+
+            for i in xrange(0, len(x)-1, num_obs_in_period):
+                start_date = x.index[i]
+                b = start_date + datetime.timedelta(days = num_days_in_period)
+                end_date = np.min(start_date.value, b.value)
+                if (end_date - start_date).days == num_days_in_period:
+                    all_data[(i / num_obs_in_period)] = x[x.index[x.index >= start_date and x.index < end_date]]
+                else:
+                    all_data[(i / num_obs_in_period)] = x[x.index[x.index >= (last_date- datetime.timedelta(days = num_days_in_period)) and x.index < last_date]]
+        else:
+            all_data[0] = x
 
 
 
